038a9eca8610472aa35f6fb1e8f4067ea374ca2c
Oscar Ramos <131794552+Elchamos64@users.noreply.github.com>
2024-05-07 09:55:15 -0600

    fixed the cart to handle adding multiple items and not override the item that was already in the cart by adding and if and else statement

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -10,8 +10,18 @@ export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
-export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+export function setLocalStorage(key, newData) {
+  // Retrieve existing data from local storage
+  let existingData = getLocalStorage(key);
+
+  // If there is existing data, append the new data to it
+  if (existingData) {
+    existingData.push(newData);
+    localStorage.setItem(key, JSON.stringify(existingData));
+  } else {
+    // If there is no existing data, create a new array with the new data and store it
+    localStorage.setItem(key, JSON.stringify([newData]));
+  }
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
