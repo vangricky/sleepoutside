86332c5e3cce2a70d884409d7c66c67c9c14a058
Ricky Vang <vangcheemeng@yahoo.com>
2024-05-16 09:28:46 -0600

    added svelte

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+  plugins: [svelte()],
   root: "src/",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -16,3 +17,5 @@ export default defineConfig({
     },
   },
 });
+
+
