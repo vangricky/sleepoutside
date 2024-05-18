356f1ad8ea30903cfd7206857327e3a370913bf4
Ricky Vang <vangcheemeng@yahoo.com>
2024-05-18 09:41:46 -0600

    team activity 4 done

---
+++ src/js/components/ProductList.svelte
@@ -0,0 +1,15 @@
+<script>
+import { getData } from "../productData.mjs";
+import ProductSummary from './ProductSummary.svelte';
+
+export let category;
+let promise = getData(category);
+
+</script>
+
+<h2>Top Products: {category}</h2>
+{#await promise}
+  Loading
+{:then products}
+<ProductSummary {products}/>
+{/await} 
---
+++ src/js/components/ProductSummary.svelte
@@ -0,0 +1,20 @@
+<script>
+export let products = {};
+let numberToDisplay = 4;
+$: products = products.slice(0, numberToDisplay);
+</script>
+
+<ul class="product-list">
+{#each products as product}
+
+<li class="product-card">
+    <a href={product.Id}>
+        <img
+        src={product.Image}
+        alt={product.Name}/>
+        <h3 class="card__brand">{product.Brand.Name}</h3>
+        <h2 class="card__name">{product.NameWithoutBrand}</h2>
+        <p class="product-card__price">{product.FinalPrice}</p></a>
+    </li>
+{/each}
+</ul> 
