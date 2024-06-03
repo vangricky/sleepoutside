ffdcbb2f8e44a3ce49bc1bada0bd032925feb0ea
Ricky Vang <vangcheemeng@yahoo.com>
2024-06-03 17:05:08 -0600

    changed the linked js file in product-list/index.html

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,11 +1,13 @@
 import ProductList from "./components/ProductList.svelte";
-import {renderHeaderFooter} from "./utils.mjs";
+import {getParam, renderHeaderFooter} from "./utils.mjs";
+import {getData} from "./productData.mjs"
 
 
 renderHeaderFooter();
 
+const category = getParam("category")
 
 new ProductList({
     target: document.querySelector(".products"),
-    props: { category: "tents" },
+    props: { category: category },
 });
